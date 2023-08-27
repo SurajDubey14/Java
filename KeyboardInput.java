63165a65cb956075bbda4b470fdd8041d136d521
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-08-27 23:50:51 +0530

    Add files via upload

---
+++ KeyboardInput.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class KeyboardInput {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        float b=sc.nextFloat();
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
