33c8d329db138aa592cd3a7cb9179e2a021ece11
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-08-29 06:10:12 +0530

    Add files via upload

---
+++ StaticVariable.java
@@ -0,0 +1,13 @@
+package Variable;
+
+class Student{
+    public static String name="Dubey";
+    public static int roll;
+}
+public class StaticVariable {
+    public static void main(String args[]) {
+        //Student.roll=10;
+        System.out.println(Student.name);
+        //System.out.println(Student.roll);
+    }
+}
