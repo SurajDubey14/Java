5b022f41bcb7243222a3ed0d141ee0da55cabdd9
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-09-01 23:37:15 +0530

    Add files via upload

---
+++ WideningTypeCasting.java
@@ -0,0 +1,12 @@
+package TypeCasting;
+
+public class WideningTypeCasting {
+    public static void main(String[] args) {
+        long i=216935;
+        long l=i;
+        double d=l;
+        System.out.println(i);
+        System.out.println(l);
+        System.out.println(d);
+    }
+}
