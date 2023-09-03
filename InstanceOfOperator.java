a77ceb666a7e420124517f9401a4fbc329be11ca
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-09-03 23:10:33 +0530

    Add files via upload

---
+++ InstanceOfOperator.java
@@ -0,0 +1,14 @@
+package Operator;
+
+class rahul{
+
+}
+
+public class InstanceOfOperator {
+    public static void main(String[] args) {
+        rahul r=new rahul();
+        rahul a=null;
+        System.out.println(r instanceof rahul);
+        System.out.println(a instanceof rahul);
+    }
+}
