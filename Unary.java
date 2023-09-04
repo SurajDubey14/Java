19c9c87723481378389c286655303b688cd64108
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-09-04 23:39:29 +0530

    Add files via upload

---
+++ Unary.java
@@ -0,0 +1,19 @@
+package Operator;
+
+import java.util.*;
+public class Unary {
+    public static void main(String[] args) {
+        //minus operator
+        int n=10;
+        System.out.println(-n);
+
+        //Unary NOT(!) operator
+        int a=10;
+        int b=20;
+        System.out.println(a<b);
+        System.out.println(!(a<b));
+
+        //Bitwise Complement(~)
+        
+    }
+}
