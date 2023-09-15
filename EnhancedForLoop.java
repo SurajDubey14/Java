8a3d79f972b8062f6025d4c4267e53e429439bab
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-09-15 23:19:05 +0530

    Add files via upload

---
+++ EnhancedForLoop.java
@@ -0,0 +1,17 @@
+package Iteration;
+
+public class EnhancedForLoop {
+    public static void main(String[] args) {
+        String arr[]={"Rahul","Shyam","Suraj"};
+        for (String a:arr){
+            System.out.println(a);
+        }
+
+        /*
+        int arr[]={1,2,2,3,4,5,6};
+        for (int a:arr){
+            System.out.println(a);
+        }
+        */
+    }
+}
