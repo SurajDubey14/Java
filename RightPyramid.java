da99b136f1252a0223910c92058461bc6ec159fb
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-09-28 23:57:50 +0530

    Add files via upload

---
+++ RightPyramid.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class RightPyramid {
+    public static void main(String[] args) {
+        int n=4;
+        int m=4;
+        for(int i=1;i<=n;i++){
+            for(int k=1;k<=(n-i);k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
