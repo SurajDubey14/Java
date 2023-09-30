cdf7fd3dbead790e14d0a093ca4c338282edd01b
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-10-01 00:28:39 +0530

    Add files via upload

---
+++ SolidRhombus.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class SolidRhombus {
+    public static void main(String[] args) {
+        int n=5;
+        for(int i=1;i<=n;i++){
+            int spaces=(n-i);
+            for(int k=1;k<=spaces;k++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=n;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
