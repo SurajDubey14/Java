7266bede5c16522a1c5caccd7e60d6caf79add7a
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-10-07 02:04:29 +0530

    Add files via upload

---
+++ UserDefined1.java
@@ -0,0 +1,19 @@
+//NO ARGUMENT ARE PASSED OR NO VALUE RETURN
+
+package Method;
+
+public class UserDefined1 {
+
+    public static void CheckEvenOdd(){
+            int a=24;
+            if(a%2==0){
+                System.out.println("The number is even");
+            }else{
+                System.out.println("Number is odd");
+            }
+    }
+    public static void main(String[] args) {
+        Maximum mm=new Maximum();
+    }
+
+}
