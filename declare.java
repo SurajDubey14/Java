e306642d14846f46d9ffd022578774a9db310696
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-11-11 00:07:13 +0530

    Add files via upload

---
+++ declare.java
@@ -0,0 +1,26 @@
+package String;
+import java.util.*;
+public class declare {
+    public static void main(String[] args) {
+        char a[]={'a','b','c'};
+        String str="abcd";
+
+        Scanner s= new Scanner(System.in);
+////        String name = s.next(); //Tony Stark mai tony hi print hoga
+//        String name1= s.nextLine(); // ye puri line or sentence lega
+////        System.out.println(name);
+//        System.out.println(name1);
+//        System.out.println(name1.length()); // To find length
+
+
+        String FirstName="Suraj";
+        String LastName="Dubey";
+
+
+
+        String FullName=FirstName+" "+LastName;
+        System.out.println(FullName);
+
+        System.out.println(FullName.charAt(1));
+    }
+}
