423173d35dd33323844e8c729a7e429397885032
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-11-17 05:06:10 +0530

    Add files via upload

---
+++ StringAsInput.java
@@ -0,0 +1,15 @@
+package String;
+
+import java.util.*;
+public class StringAsInput {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        String str=sc.nextLine();
+        System.out.println(str);
+
+        String st= "abcd";
+        String str1= new String("xyz");
+        System.out.println(st);
+        System.out.println(str1);
+    }
+}
