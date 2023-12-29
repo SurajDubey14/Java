f0129565a63f399077923f96085135f639a1d7db
Suraj Dubey <139530741+SurajDubey14@users.noreply.github.com>
2023-12-29 22:25:32 +0530

    Add files via upload

---
+++ Suraj.java
@@ -0,0 +1,22 @@
+package Lecture1;
+
+// This is first java program
+public class Suraj {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
+
+/*
+
+    java works on unicode (UTF-8) i.e. integer value of 4 bytes.
+    name should be precise and short.
+
+
+
+    variables , identifier , datatype , literals etc
+
+
+
+
+*/
